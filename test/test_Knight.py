c7035f23315d125d4b7d76129f6e0ebdd47a5f38
davidovits1 <david.o3111@gmail.com>
2023-05-04 01:45:21 +0300

    add test to get_valid_piece_takes

--- test/test_Knight.py
+++ test/test_Knight.py
@@ -1,7 +1,7 @@
 import pytest
 from unittest.mock import patch
 import unittest
-from Piece import Knight, Player
+from Piece import Knight, Player, Piece
 
 
 class TestKnight(unittest.TestCase):
@@ -40,6 +40,52 @@ class TestKnight(unittest.TestCase):
             (2, 1), (1, 2)
         ]
         actual_moves = knight.get_valid_peaceful_moves(game_state_mock)
+        self.assertCountEqual(actual_moves, expected_moves)
+
+    @patch('chess_engine.game_state')
+    def test_get_valid_piece_takes_nowhere_to_move(self, MockGameState):
+        def get_piece_side_effect(row, col):
+            return Piece('p', row, col, Player.PLAYER_1)
+
+        game_state_mock = MockGameState()
+        game_state_mock.get_piece.side_effect = get_piece_side_effect
+
+        knight = Knight("N", 3, 3, Player.PLAYER_1)
+        expected_moves = []
+        actual_moves = knight.get_valid_piece_takes(game_state_mock)
+        self.assertCountEqual(actual_moves, expected_moves)
+
+    @patch('chess_engine.game_state')
+    def test_get_valid_piece_takes_one_place_to_move(self, MockGameState):
+        def get_piece_side_effect(row, col):
+            if row == 1 and col == 2:
+                return Piece('p', 1, 2, Player.PLAYER_2)
+            else:
+                return Piece('p', row, col, Player.PLAYER_1)
+
+        game_state_mock = MockGameState()
+        game_state_mock.get_piece.side_effect = get_piece_side_effect
+
+        knight = Knight("N", 3, 3, Player.PLAYER_1)
+        expected_moves = [
+            (1, 2)
+        ]
+        actual_moves = knight.get_valid_piece_takes(game_state_mock)
+        self.assertCountEqual(actual_moves, expected_moves)
+
+    @patch('chess_engine.game_state')
+    def test_get_valid_piece_takes_all_place_to_move(self, MockGameState):
+        def get_piece_side_effect(row, col):
+            return Piece('p', row, col, Player.PLAYER_2)
+
+        game_state_mock = MockGameState()
+        game_state_mock.get_piece.side_effect = get_piece_side_effect
+
+        knight = Knight("N", 3, 3, Player.PLAYER_1)
+        expected_moves = [
+            (1, 2), (1, 4), (2, 1), (2, 5), (4, 1), (4, 5), (5, 4), (5, 2)
+        ]
+        actual_moves = knight.get_valid_piece_takes(game_state_mock)
         self.assertCountEqual(actual_moves, expected_moves)
 
 
